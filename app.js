4dfd94b22cb3f0f02a4f4f817bbe75d73277521e
Marcos Rigo <marcos.rigo.10@gmail.com>
2023-10-11 12:05:37 -0300

    Comienzo proyecto js

---
+++ app.js
@@ -0,0 +1,24 @@
+// alert("Esto es una alerta");
+
+// console.log("Probando probando probando");
+
+// let edad = prompt("edad?");
+// console.log("Tu edad es ", edad);
+// document.write('Tu "edad" es ', edad);
+
+let string = "Hola estoy probando un string y sus propiedades";
+
+let longitud = string.length;
+console.log("Length de string es ", longitud);
+
+console.log(string.toUpperCase());
+
+console.log(string.toLocaleLowerCase());
+
+console.log(string.includes("marcos"));
+
+console.log(string.charAt(2));
+
+console.log(string.substring(0, 4));
+
+console.log(string.substring(10).replace(" ", "+"));
