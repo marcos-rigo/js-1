d8c315926a6656b2f0630b85d3d3445f27c6f991
Marcos Rigo <marcos.rigo.10@gmail.com>
2023-10-17 19:57:14 -0300

    carrito de compras

--- app.js
+++ app.js
@@ -22,3 +22,68 @@ console.log(string.charAt(2));
 console.log(string.substring(0, 4));
 
 console.log(string.substring(10).replace(" ", "+"));
+
+let days = [
+  "lunes",
+  "martes",
+  "miercoles",
+  "jueves",
+  "viernes",
+  "sabado",
+  "domingo",
+];
+
+console.log(days[3]);
+
+// FUNCIONES CALLBACK
+const edades = [22, 33, 55, 20, 16];
+
+// function mayoresDe25(edad) {
+//   if (edad >= 25) {
+//     return edad;
+//   }
+// }
+
+// const olderThan25 = edades.filter(mayoresDe25);
+const olderThan25 = edades.filter((edad) => edad >= 25); //devuelve array que cumple con la condicion y si ninguno coincude te devuelve un array vacio
+
+const mayores25 = edades.find((edad) => edad >= 25); // devuelve el primer y unico elemento con el q cumple la condicion
+
+//forEach no devuelve nada
+// edades.forEach((edad) => {
+//   if (edad > 25) console.log("Es mayor q 25");
+//   else console.log("Es menor que 25");
+// });
+
+//map devuelve array
+// const sumarAños = edades.map((edad) => edad + 10);
+
+// Carrito
+let productos = [
+  "notebook",
+  "tv",
+  "parlante",
+  "proyector",
+  "microfono",
+  "mouse",
+  "teclado",
+  "multiuncion",
+];
+let carrito = [];
+
+const addProduct = () => {
+  const productToAdd = prompt(
+    "que producto agregas al carrito?"
+  ).toLocaleLowerCase();
+  if (productos.includes(productToAdd)) {
+    carrito.push(productToAdd);
+  } else {
+    alert("no tenemos ese producto");
+  }
+};
+
+const getCarrito = () => {
+  alert("los elementos de su carrito son \n- " + carrito.join("\n- "));
+};
+
+const getProduct = () => {};
